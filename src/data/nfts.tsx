51abc195299547b1582f089435a3b4e28c5cd113
RealToma <real.toma.contact@gmail.com>
2024-07-09 03:08:01 -0400

    updated nfts

--- src/data/nfts.tsx
+++ src/data/nfts.tsx
@@ -25,18 +25,38 @@ export const dataNFTsGallery = [
     img: "/assets/images/nfts/05.jpg",
   },
   {
-    name: "NFT#02",
-    description: "This is NFT#02.",
+    name: "NFT#06",
+    description: "This is NFT#06.",
     img: "/assets/images/nfts/02.jpg",
   },
   {
-    name: "NFT#03",
-    description: "This is NFT#03.",
+    name: "NFT#07",
+    description: "This is NFT#07.",
     img: "/assets/images/nfts/03.jpg",
   },
   {
-    name: "NFT#04",
-    description: "This is NFT#04.",
+    name: "NFT#08",
+    description: "This is NFT#08.",
+    img: "/assets/images/nfts/04.jpg",
+  },
+  {
+    name: "NFT#09",
+    description: "This is NFT#09.",
+    img: "/assets/images/nfts/03.jpg",
+  },
+  {
+    name: "NFT#10",
+    description: "This is NFT#10.",
+    img: "/assets/images/nfts/02.jpg",
+  },
+  {
+    name: "NFT#11",
+    description: "This is NFT#11.",
+    img: "/assets/images/nfts/01.jpg",
+  },
+  {
+    name: "NFT#12",
+    description: "This is NFT#12.",
     img: "/assets/images/nfts/04.jpg",
   },
 ];
